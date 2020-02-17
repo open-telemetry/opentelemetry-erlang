5023b95bf3969b5acfce74bee6a1255640e92eb5
Tristan Sloughter <t@crashfast.com>
2020-02-17 09:18:15 -0700

    fixing up mix.lock and use of event (#23)
    * update elixir opentelemetry api to replace time events with events
    
    * update mix lock file

--- lib/open_telemetry.ex
+++ lib/open_telemetry.ex
@@ -4,8 +4,7 @@ defmodule OpenTelemetry do
   defdelegate links(link_list), to: :opentelemetry
   defdelegate link(trace_id, span_id, attributes, trace_state), to: :opentelemetry
   defdelegate event(name, attributes), to: :opentelemetry
-  defdelegate timed_event(timestamp, name, attributes), to: :opentelemetry
-  defdelegate timed_event(timestamp, event), to: :opentelemetry
-  defdelegate timed_events(timed_event_list), to: :opentelemetry
+  defdelegate event(timestamp, name, attributes), to: :opentelemetry
+  defdelegate events(event_list), to: :opentelemetry
   defdelegate status(code, message), to: :opentelemetry
 end
