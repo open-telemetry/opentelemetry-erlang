3ddb4720d0da55b5da24106089d3386aa75b34b4
bryannaegele <bryan@fourthtime.com>
2020-08-20 11:57:10 -0600

    Add env var usage to test

--- test/opentelemetry_exporter_SUITE.erl
+++ test/opentelemetry_exporter_SUITE.erl
@@ -125,6 +125,7 @@ span_round_trip(_Config) ->
 
 %% insert a couple spans and export to locally running otel collector
 verify_export(Config) ->
+    os:putenv("OTEL_RESOURCE_LABELS", "service.name=my-test-service,service.version=98da75ea6d38724743bf42b45565049238d86b3f"),
     Protocol = ?config(protocol, Config),
     Port = case Protocol of
                grpc ->
@@ -180,8 +181,8 @@ verify_export(Config) ->
 
     ?assertMatch([#{instrumentation_library := undefined,
                     spans := [_, _]}], opentelemetry_exporter:to_proto_by_instrumentation_library(Tid)),
-
-    ?assertMatch(ok, opentelemetry_exporter:export(Tid, ot_resource:create([{"service.name",
-                                                                             "my-test-service"}]), State)),
+    Resource = ot_resource_env_var:get_resource(),
+    ?assertMatch({ot_resource,[{<<"service.name">>,<<"my-test-service">>},{<<"service.version">>,<<"98da75ea6d38724743bf42b45565049238d86b3f">>}]}, Resource),
+    ?assertMatch(ok, opentelemetry_exporter:export(Tid, Resource, State)),
 
     ok.
