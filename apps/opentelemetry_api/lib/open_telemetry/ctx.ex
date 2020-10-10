d118fbd4ce18a68dda245945d49aa9077810ca2c
Tristan Sloughter <t@crashfast.com>
2020-10-10 08:28:31 -0600

    add Elixir modules for baggage and context

---
+++ apps/opentelemetry_api/lib/open_telemetry/ctx.ex
@@ -0,0 +1,18 @@
+defmodule OpenTelemetry.Ctx do
+  @moduledoc """
+  Ctx is responsible for propagating values within a process that are associated
+  with a particular Trace or set of Baggage. `OpenTelemetry.Tracer` and
+  `OpenTelemetry.Baggage` handle updating the Context.
+  """
+
+  defdelegate new(), to: :otel_ctx
+  defdelegate attach(ctx), to: :otel_ctx
+  defdelegate detach(token), to: :otel_ctx
+  defdelegate set_value(key, value), to: :otel_ctx
+  defdelegate set_value(ctx, key, value), to: :otel_ctx
+  defdelegate get_value(key, default), to: :otel_ctx
+  defdelegate get_value(ctx, key, default), to: :otel_ctx
+  defdelegate clear(), to: :otel_ctx
+  defdelegate remove(key), to: :otel_ctx
+  defdelegate get_current(), to: :otel_ctx
+end
