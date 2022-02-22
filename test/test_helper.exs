7e45530c3ed2c761de053c5d485516344b866444
Tristan Sloughter <tsloughter@splunk.com>
2022-02-22 11:35:56 -0700

    elixir sdk tests: no need to start exporter bc tests use pid exporter

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -5,6 +5,6 @@ Application.put_env(:opentelemetry, :processors, [
   {:otel_batch_processor, %{scheduled_delay_ms: 1}}
 ])
 
-{:ok, _} = Application.ensure_all_started(:opentelemetry_exporter)
+{:ok, _} = Application.ensure_all_started(:opentelemetry)
 
 ExUnit.start()
