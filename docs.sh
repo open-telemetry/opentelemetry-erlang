f8965567c16425a2085d084e45b3b4c81d4d298e
Tristan Sloughter <tsloughter@splunk.com>
2022-02-22 14:21:01 -0700

    add zipkin docs generation

--- docs.sh
+++ docs.sh
@@ -9,23 +9,31 @@ set -e
 
 rebar3 compile
 rebar3 edoc
-version=1.0.1
+sdk_version=1.0.2
+api_version=1.0.2
+otlp_version=1.0.2
+zipkin_version=1.0.0
 
-ex_doc "opentelemetry" $version "_build/default/lib/opentelemetry/ebin" \
-  --source-ref v${version} \
+ex_doc "opentelemetry" $sdk_version "_build/default/lib/opentelemetry/ebin" \
+  --source-ref v${sdk_version} \
   --config apps/opentelemetry/docs.config $@ \
   --output "apps/opentelemetry/doc"
 
-ex_doc "opentelemetry_exporter" $version "_build/default/lib/opentelemetry_exporter/ebin" \
-  --source-ref v${version} \
+ex_doc "opentelemetry_exporter" $otlp_version "_build/default/lib/opentelemetry_exporter/ebin" \
+  --source-ref v${otlp_version} \
   --config apps/opentelemetry_exporter/docs.config $@ \
   --output "apps/opentelemetry_exporter/doc"
+
+ex_doc "opentelemetry_zipkin" $zipkin_version "_build/default/lib/opentelemetry_zipkin/ebin" \
+  --source-ref v${zipkin_version} \
+  --config apps/opentelemetry_zipkin/docs.config $@ \
+  --output "apps/opentelemetry_zipkin/doc"
 
 pushd apps/opentelemetry_api
 mix deps.get
 mix compile
 popd
-ex_doc "opentelemetry_api" $version "apps/opentelemetry_api/_build/dev/lib/opentelemetry_api/ebin" \
-  --source-ref v${version} \
+ex_doc "opentelemetry_api" $api_version "apps/opentelemetry_api/_build/dev/lib/opentelemetry_api/ebin" \
+  --source-ref v${api_version} \
   --config apps/opentelemetry_api/docs.config $@ \
   --output "apps/opentelemetry_api/doc"
