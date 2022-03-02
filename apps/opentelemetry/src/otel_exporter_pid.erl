c74bb8516f85117bc8435926ab84dfeb8acc5f3c
fr0stbyte <radu@brex.com>
2022-03-02 12:04:59 -0500

    3

--- apps/opentelemetry/src/otel_exporter_pid.erl
+++ apps/opentelemetry/src/otel_exporter_pid.erl
@@ -27,6 +27,7 @@ init(Pid) ->
     {ok, Pid}.
 
 export(SpansTid, _Resource, Pid) ->
+    io:format("~p table info: ~p~n", [SpansTid, ets:info(SpansTid)]),
     ets:foldl(fun(Span, _Acc) ->
                       io:format("processing spans : ~p~n", [Span]),
                       Pid ! {span, Span}
