5246ad834b319a9122262c75c1bb77eda1806a59
Tristan Sloughter <tsloughter@splunk.com>
2022-09-05 12:13:38 -0600

    add observation type for return of observable callback

--- apps/opentelemetry_api_experimental/src/otel_instrument.erl
+++ apps/opentelemetry_api_experimental/src/otel_instrument.erl
@@ -29,9 +29,11 @@
                 ?KIND_OBSERVABLE_GAUGE | ?KIND_UPDOWN_COUNTER | ?KIND_OBSERVABLE_UPDOWNCOUNTER.
 -type unit() :: atom(). %% latin1, maximum length of 63 characters
 -type value_type() :: ?VALUE_TYPE_INTEGER | ?VALUE_TYPE_FLOAT.
+-type observation() :: {number(), opentelemetry:attributes_map()}.
+-type named_observation() :: {name(), number(), opentelemetry:attributes_map()}.
 -type callback_args() :: term().
--type callback() :: fun((callback_args()) -> {number(), opentelemetry:attributes_map()} |
-                                             [{name(), number(), opentelemetry:attributes_map()}]).
+-type callback() :: fun((callback_args()) -> observation() |
+                                             [named_observation()]).
 
 -type t() :: #instrument{}.
 
