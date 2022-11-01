fce299bc97afa9876e8281b4ae65cd6821402878
Tristan Sloughter <t@crashfast.com>
2022-11-01 17:48:58 -0600

    fix formatting in elixir file

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,8 +2,8 @@ Application.stop(:opentelemetry)
 Application.load(:opentelemetry)
 
 Application.put_env(:opentelemetry, :processors, [
-      {:otel_batch_processor, %{scheduled_delay_ms: 1,
-                                exporter: {:otel_exporter_pid, :test_registered_pid}}}
+  {:otel_batch_processor,
+   %{scheduled_delay_ms: 1, exporter: {:otel_exporter_pid, :test_registered_pid}}}
 ])
 
 {:ok, _} = Application.ensure_all_started(:opentelemetry)
