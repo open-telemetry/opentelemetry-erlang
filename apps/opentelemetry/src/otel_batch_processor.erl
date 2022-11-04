7dc65760c5fafb6aaaa816c004591e4b499b50c4
Tristan Sloughter <t@crashfast.com>
2022-11-04 05:22:24 -0600

    add deprecated module attribute for set_exporter

--- apps/opentelemetry/src/otel_batch_processor.erl
+++ apps/opentelemetry/src/otel_batch_processor.erl
@@ -47,6 +47,10 @@
          exporting/3,
          terminate/3]).
 
+-deprecated({set_exporter, 1, "set through the otel_tracer_provider instead"}).
+-deprecated({set_exporter, 2, "set through the otel_tracer_provider instead"}).
+-deprecated({set_exporter, 3, "set through the otel_tracer_provider instead"}).
+
 -include_lib("opentelemetry_api/include/opentelemetry.hrl").
 -include_lib("kernel/include/logger.hrl").
 -include("otel_span.hrl").
