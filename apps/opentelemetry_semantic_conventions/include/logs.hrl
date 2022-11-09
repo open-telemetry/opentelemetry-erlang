e9a60f0879ad23e7b5933e263c3ca9cd65a6a0eb
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2022-11-09 15:48:51 -0600

    feat: add elixir semantic conventions

--- apps/opentelemetry_semantic_conventions/include/logs.hrl
+++ apps/opentelemetry_semantic_conventions/include/logs.hrl
@@ -1,11 +1,11 @@
 %% The schema url for telemetry resources
--define(SCHEMA_URL, <<"https://opentelemetry.io/schemas/1.13.0">>).
+-define(LOGS_SCHEMA_URL, <<"https://opentelemetry.io/schemas/1.13.0">>).
 
 %% The name identifies the event
 -define(EVENT_NAME, 'event.name'),
 
 %% The domain identifies the context in which an event happened. An event name is unique only within a domain
 %% An `event.name` is supposed to be unique only in the context of an
-      %%  `event.domain`, so this allows for two events in different domains to
-      %%  have same `event.name`, yet be unrelated events
+%% `event.domain`, so this allows for two events in different domains to
+%% have same `event.name`, yet be unrelated events
 -define(EVENT_DOMAIN, 'event.domain'),
---
+++ apps/opentelemetry_semantic_conventions/test/open_telemetry/semantic_conventions/logs_test.exs
@@ -0,0 +1,4 @@
+defmodule OpenTelemetry.SemanticConventions.LogsTest do
+  use ExUnit.Case, async: true
+  doctest OpenTelemetry.SemanticConventions.Logs
+end
