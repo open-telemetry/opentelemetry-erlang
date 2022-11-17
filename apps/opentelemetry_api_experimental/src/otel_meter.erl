f29cd82949f90435d27bc12f676fc07981589de9
Tristan Sloughter <t@crashfast.com>
2022-11-17 14:43:36 -0700

    remove doc that creating instrment has no side effect since it does now

--- apps/opentelemetry_api_experimental/src/otel_meter.erl
+++ apps/opentelemetry_api_experimental/src/otel_meter.erl
@@ -12,8 +12,7 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 %%
-%% @doc `otel_meter' is responsible for creating Instruments. An Instrument
-%% is just a record so calling the creation function has no side effects.
+%% @doc `otel_meter' is responsible for creating Instruments.
 %% @end
 %%%-------------------------------------------------------------------------
 -module(otel_meter).
