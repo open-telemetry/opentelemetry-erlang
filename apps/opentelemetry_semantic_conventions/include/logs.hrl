493b302e4e37a4c86d99607d5b2cc8e1478f1c9a
Tristan Sloughter <t@crashfast.com>
2022-11-21 10:51:18 -0700

    fix Erlang header template to end define's in periods

--- apps/opentelemetry_semantic_conventions/include/logs.hrl
+++ apps/opentelemetry_semantic_conventions/include/logs.hrl
@@ -2,10 +2,10 @@
 -define(LOGS_SCHEMA_URL, <<"https://opentelemetry.io/schemas/1.13.0">>).
 
 %% The name identifies the event
--define(EVENT_NAME, 'event.name'),
+-define(EVENT_NAME, 'event.name').
 
 %% The domain identifies the context in which an event happened. An event name is unique only within a domain
 %% An `event.name` is supposed to be unique only in the context of an
 %% `event.domain`, so this allows for two events in different domains to
 %% have same `event.name`, yet be unrelated events
--define(EVENT_DOMAIN, 'event.domain'),
+-define(EVENT_DOMAIN, 'event.domain').
