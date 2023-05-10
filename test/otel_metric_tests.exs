0e6f57b55f86a92e3c83f333136d3b0179df73f4
Tristan Sloughter <t@crashfast.com>
2023-05-10 16:58:21 -0600

    add Elixir UpDownCounter test

--- test/otel_metric_tests.exs
+++ test/otel_metric_tests.exs
@@ -2,10 +2,17 @@ defmodule OtelMetricTests do
   use ExUnit.Case, async: true
 
   require OpenTelemetryAPIExperimental.Counter, as: Counter
+  require OpenTelemetryAPIExperimental.UpDownCounter, as: UpDownCounter
 
   require Record
   @fields Record.extract(:metric, from_lib: "opentelemetry_experimental/include/otel_metrics.hrl")
   Record.defrecordp(:metric, @fields)
+
+  @fields Record.extract(:datapoint, from_lib: "opentelemetry_experimental/include/otel_metrics.hrl")
+  Record.defrecordp(:datapoint, @fields)
+
+  @fields Record.extract(:sum, from_lib: "opentelemetry_experimental/include/otel_metrics.hrl")
+  Record.defrecordp(:sum, @fields)
 
   setup do
     Application.load(:opentelemetry_experimental)
@@ -33,6 +40,29 @@ defmodule OtelMetricTests do
 
     :otel_meter_server.force_flush()
 
-    assert_receive {:metric, metric(name: :counter_a)}
+    assert_receive {:metric, metric(name: :counter_a,
+                                    data: sum(datapoints: [datapoint(value: 1)]))}
+  end
+
+  test "create UpDownCounter with macros" do
+    UpDownCounter.create(:updown_counter_a, %{unit: "1", description: "some updown_counter_a"})
+    UpDownCounter.add(:updown_counter_a, 5, [])
+    UpDownCounter.add(:updown_counter_a, -5, [])
+
+    :otel_meter_server.force_flush()
+    assert_receive {:metric, metric(name: :updown_counter_a,
+                                    data: sum(datapoints: [datapoint(value: 0)]))}
+
+    UpDownCounter.add(:updown_counter_a, -3, [])
+
+    :otel_meter_server.force_flush()
+    assert_receive {:metric, metric(name: :updown_counter_a,
+                                    data: sum(datapoints: [datapoint(value: -3)]))}
+
+    UpDownCounter.add(:updown_counter_a, 9, [])
+
+    :otel_meter_server.force_flush()
+    assert_receive {:metric, metric(name: :updown_counter_a,
+                                    data: sum(datapoints: [datapoint(value: 9)]))}
   end
 end
