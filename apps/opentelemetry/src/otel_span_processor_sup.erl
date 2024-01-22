d5fe7e00fcf658984b74d431e723007559365318
Andrea Leopardi <an.leopardi@gmail.com>
2024-01-22 12:12:47 +0100

    Add docs to *_processor modules

--- apps/opentelemetry/src/otel_span_processor_sup.erl
+++ apps/opentelemetry/src/otel_span_processor_sup.erl
@@ -12,7 +12,7 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 %%
-%% @doc
+%% @private
 %% @end
 %%%-------------------------------------------------------------------------
 -module(otel_span_processor_sup).
