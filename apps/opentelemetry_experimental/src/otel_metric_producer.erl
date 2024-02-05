3b40d4e2e063c494d19659a942231d012cf813c6
Tristan Sloughter <t@crashfast.com>
2024-02-05 04:01:37 -0700

    add metric producer behaviour

---
+++ apps/opentelemetry_experimental/src/otel_metric_producer.erl
@@ -0,0 +1,42 @@
+%%%------------------------------------------------------------------------
+%% Copyright 2023, OpenTelemetry Authors
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% @doc
+%% @end
+%%%-------------------------------------------------------------------------
+-module(otel_metric_producer).
+
+-include("otel_metrics.hrl").
+
+-export([new/2,
+         produce_batch/1]).
+
+-callback new(module(), term()) -> t().
+-callback produce_batch(t()) -> [#metric{}].
+
+-record(producer, {module :: module(),
+                   state  :: term()}).
+-type t() :: #producer{}.
+
+-export_type([t/0]).
+
+new(Module, Config) ->
+    State = Module:new(Config),
+    #producer{module=Module,
+              state=State}.
+
+-spec produce_batch(t()) -> [tuple()].
+produce_batch(#producer{module=Module,
+                        state=State}) ->
+    Module:produce_batch(State).
