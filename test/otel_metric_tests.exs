e42deeae603b22ab6fdc1abf766826bc43a24efe
Tristan Sloughter <tristan@sloughter.dev>
2024-03-16 11:01:47 -0600

    mix format

--- test/otel_metric_tests.exs
+++ test/otel_metric_tests.exs
@@ -17,7 +17,9 @@ defmodule OtelMetricTests do
   @fields Record.extract(:sum, from_lib: "opentelemetry_experimental/include/otel_metrics.hrl")
   Record.defrecordp(:sum, @fields)
 
-  @fields Record.extract(:histogram, from_lib: "opentelemetry_experimental/include/otel_metrics.hrl")
+  @fields Record.extract(:histogram,
+            from_lib: "opentelemetry_experimental/include/otel_metrics.hrl"
+          )
   Record.defrecordp(:histogram, @fields)
 
   setup do
@@ -111,9 +113,31 @@ defmodule OtelMetricTests do
     assert_receive {:metric,
                     metric(
                       name: :histogram_a,
-                      data: histogram(aggregation_temporality: :temporality_cumulative,
-                        datapoints: [{:histogram_datapoint, %{}, _, _, 1, 1, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                      [0.0, 5.0, 10.0, 25.0, 50.0, 75.0, 100.0, 250.0, 500.0, 750.0, 1000.0, 2500.0, 5000.0, 7500.0, 10000.0], [], 0, 1, 1}])
+                      data:
+                        histogram(
+                          aggregation_temporality: :temporality_cumulative,
+                          datapoints: [
+                            {:histogram_datapoint, %{}, _, _, 1, 1,
+                             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [
+                               0.0,
+                               5.0,
+                               10.0,
+                               25.0,
+                               50.0,
+                               75.0,
+                               100.0,
+                               250.0,
+                               500.0,
+                               750.0,
+                               1000.0,
+                               2500.0,
+                               5000.0,
+                               7500.0,
+                               10000.0
+                             ], [], 0, 1, 1}
+                          ]
+                        )
                     )}
 
     Histogram.record(:histogram_a, 10)
@@ -123,10 +147,31 @@ defmodule OtelMetricTests do
     assert_receive {:metric,
                     metric(
                       name: :histogram_a,
-                      data: histogram(aggregation_temporality: :temporality_cumulative,
-                        datapoints: [{:histogram_datapoint, %{}, _, _, 2, 11, [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                      [0.0, 5.0, 10.0, 25.0, 50.0, 75.0, 100.0, 250.0, 500.0, 750.0, 1000.0, 2500.0, 5000.0, 7500.0, 10000.0], [], 0, 1, 10}])
+                      data:
+                        histogram(
+                          aggregation_temporality: :temporality_cumulative,
+                          datapoints: [
+                            {:histogram_datapoint, %{}, _, _, 2, 11,
+                             [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [
+                               0.0,
+                               5.0,
+                               10.0,
+                               25.0,
+                               50.0,
+                               75.0,
+                               100.0,
+                               250.0,
+                               500.0,
+                               750.0,
+                               1000.0,
+                               2500.0,
+                               5000.0,
+                               7500.0,
+                               10000.0
+                             ], [], 0, 1, 10}
+                          ]
+                        )
                     )}
   end
-
 end
