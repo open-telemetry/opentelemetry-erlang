0564be227333e0ff0c7ec29c408c29b43a6dfd30
Konrad Zemek <konrad@v7labs.com>
2025-07-23 11:36:23 +0200

    Use `binary:encode_hex/2` instead of `io_lib:format/2`
    `io_lib:format/2` has very significant overhead over
    `binary:encode_hex/2`.

--- apps/opentelemetry_api/src/otel_span.erl
+++ apps/opentelemetry_api/src/otel_span.erl
@@ -127,21 +127,11 @@ hex_span_ctx(_) ->
 
 -spec hex_trace_id(opentelemetry:span_ctx()) -> opentelemetry:hex_trace_id().
 hex_trace_id(#span_ctx{trace_id=TraceId}) ->
-    case otel_utils:format_binary_string("~32.16.0b", [TraceId]) of
-        {ok, Binary} ->
-            Binary;
-        _ ->
-            <<>>
-    end.
+    binary:encode_hex(<<TraceId:128>>, lowercase).
 
 -spec hex_span_id(opentelemetry:span_ctx()) -> opentelemetry:hex_span_id().
 hex_span_id(#span_ctx{span_id=SpanId}) ->
-    case otel_utils:format_binary_string("~16.16.0b", [SpanId]) of
-        {ok, Binary} ->
-            Binary;
-        _ ->
-            <<>>
-    end.
+    binary:encode_hex(<<SpanId:64>>, lowercase).
 
 -spec tracestate(opentelemetry:span_ctx() | undefined) -> otel_tracestate:t().
 tracestate(#span_ctx{tracestate=Tracestate}) ->
